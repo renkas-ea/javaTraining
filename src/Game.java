bf80c9e374660f819bda394248f054cf05da3803
renkasea <Ekaterina.renkas@raiffeisen.ru>
2019-09-17 00:21:22 +0600

    done game

---
+++ src/Game.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Game {
+    //в задании не указан диапазон значений, но без него - никогда не угадать
+    private int randomNum = new Random().nextInt(100);
+    private Scanner scanner = new Scanner(System.in);
+
+    public void gamePlay() {
+        System.out.println(randomNum);
+        Integer prevScanNum = null;
+        while (true) {
+            System.out.println("Введите число:");
+            int scannerNum = scanner.nextInt();
+            if (prevScanNum == null) {
+                prevScanNum = scannerNum;
+                continue;
+            }
+
+            if (randomNum == scannerNum) {
+                System.out.println("Успех");
+                break;
+            }
+
+            if (Math.abs(prevScanNum - randomNum) > Math.abs(scannerNum - randomNum)) {
+                System.out.println("Горячо");
+            } else if (Math.abs(prevScanNum - randomNum) < Math.abs(scannerNum - randomNum)) {
+                System.out.println("Холодно");
+            }
+            prevScanNum = scannerNum;
+
+        }
+    }
+
+
+}
